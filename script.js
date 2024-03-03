e6764e909cf1d08f974361f066be5b50ec9118f1
Bidji <159267099+weBidji@users.noreply.github.com>
2024-03-03 09:53:44 -0500

    Added round logic

--- script.js
+++ script.js
@@ -1,42 +1,79 @@
 // the player must make a selection
-let playerSelection;
-let playerScore = 0;
 
-// the computer must make a selection
-let computerSelection = getComputerChoice();
+let playerScore = 0;
 let computerScore = 0;
+let playerChoice;
+let computerChoice;
+
+function playerSelection(){
+
+  playerChoice = prompt('Rock, paper or scissors?');
+  playerChoice = playerChoice.toLowerCase();
+
+  if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors'){
+    
+    console.log(`You picked ${playerChoice}.`)
+
+  return playerChoice;
+  
+
+  } else {
+    alert('Please enter a valid selection.');
+    
+  }
+}
+
+
 //computer selection function
 
-function getComputerChoice(){
+function computerSelection(){
 
     let randomNumber = Math.floor(Math.random()*3);
-    switch(randomNumber){
-    case 0:
-      return'rock';
-    case 1:
-      return'paper';
-    case 2:
-      return'scissors';
-  }
-  }
-
-
-// the round must be played, comparing selections
-
-function playRound (playerSelection, computerSelection){
     
-    if (playerSelection === computerSelection) {
+
+ switch(randomNumber){
+    case 0:
+      computerChoice = 'rock';
+      
+      break;
+
+    case 1:
+      computerChoice = 'paper';
+      
+      break;
+
+    case 2:
+      computerChoice = 'scissors'
+      
+  }
+  console.log(`Your opponent picked ${computerChoice}.`)
+  
+  }
+
+
+// the round must be played, comparing selections, then increase scores accordingly
+
+function playRound(){
+
+  playerSelection();
+  computerSelection();
+
+
+    
+    if (playerChoice === computerChoice) {
  
-        console.log ("It's a tie")
+        console.log ("It's a tie!")
     } else if ((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')){
-        console.log('Computer wins the round');
-        computerScore++;
+        console.log('Computer wins the round!');
+        // computerScore++;
+        // console.log(computerScore);
     } else {
-        console.log('You win the round');
-        playerScore++;
+        console.log('You win the round!');
+        // playerScore++;
+        // console.log(playerScore);
     }
 
-    console.log(`Player score: ${playerScore} | Computer Score: ${computerScore}`);
+    // console.log(`Player score: ${playerScore} | Computer Score: ${computerScore}`);
 }
 
 
