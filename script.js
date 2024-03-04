9fbb432691814b7eb0b489d753e347954dd97435
Bidji <159267099+weBidji@users.noreply.github.com>
2024-03-04 16:20:36 -0500

    removed possibility to click while round is playing

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let winner;
 let loser;
 let playerScoreCount = 0;
 let computerScoreCount = 0;
+let isRoundPlaying = false;
 const playerScore = document.getElementById('player-score');
 const computerScore = document.getElementById('cpu-score');
 playerScore.innerText = ` ${playerScoreCount}`;
@@ -28,141 +29,131 @@ let computerSelected;
 ;
 
 
-let playerPicks = [rock,paper,scissors];
+let playerPicks = [rock, paper, scissors];
 let cpuPicks = [cpuRock, cpuPaper, cpuScissors];
 
 playerPicks.forEach((pick) => {
+  pick.addEventListener('click', (event) => {
+    if (!isRoundPlaying) {
+      pick.classList.add('player-selected');
+      playerSelected = document.querySelector('.player-selected');
+      console.log(playerSelected);
 
-  pick.addEventListener('click',() => {
+      switch (pick.id) {
+        case 'rock':
+          playerChoice = 'rock';
+          break;
 
-    pick.classList.add('player-selected');
-    playerSelected = document.querySelector('.player-selected');
-    console.log(playerSelected);
+        case 'paper':
+          playerChoice = 'paper';
+          break;
 
-switch (pick.id){
-  case 'rock' :
-    playerChoice = 'rock';
-  
-  break;
+        case 'scissors':
+          playerChoice = 'scissors';
+          break;
+      }
 
-  case 'paper' :
-    playerChoice = 'paper';
-  
+      playRound();
+    } else {
+      event.preventDefault();
+    }
+  });
+});
 
-  break;
+function playRound() {
 
-  case 'scissors' :
-    playerChoice = 'scissors';
-  break;
-
-}
-  playRound();
- 
-
-})
-  
-  
-})
-
-function playRound(){
+  isRoundPlaying = true;
 
   playerPicks.forEach((pick) => {
     pick.classList.add('disabled-hover');
   });
 
-    computerSelection();
-    
+  computerSelection();
+
   console.log('you: ' + playerChoice);
   console.log('cpu: ' + computerChoice);
-      
-      if (playerChoice === computerChoice) {
-   
-          gameInfo.innerText = "It's a tie!"
-          console.log('tie');
-          computerSelected.style.borderRight = '20px solid yellow';
-          computerSelected.style.borderRadius = '100px';
-          playerSelected.style.borderLeft = '20px solid yellow';
-          playerSelected.style.borderRadius = '100px';
-  
-      } else if ((computerChoice === 'rock' && playerChoice === 'scissors') || (computerChoice=== 'scissors' && playerChoice=== 'paper') || (computerChoice === 'paper' && playerChoice === 'rock')){
-          
-        
-        gameInfo.innerText = 'Computer wins the round!';
-          computerScoreCount++;
-          console.log('cpu wins');
 
-          console.log('you: ' + playerScoreCount)
-          console.log('cpu: ' + computerScoreCount);
+  if (playerChoice === computerChoice) {
 
-          winner = computerChoice;
-          loser = playerChoice;
+    gameInfo.innerText = "It's a tie!"
+    console.log('tie');
+    computerSelected.style.borderRight = '20px solid yellow';
+    computerSelected.style.borderRadius = '100px';
+    playerSelected.style.borderLeft = '20px solid yellow';
+    playerSelected.style.borderRadius = '100px';
 
-          playerSelected.style.borderLeft = '20px solid red';
-          computerSelected.style.borderRight = '20px solid green'
-          computerSelected.style.borderRadius = '100px';
-  
-      } else {
-       
-         gameInfo.innerText = 'You win the round!';
-          playerScoreCount++;
-          console.log('u win');
-
-          console.log('you: ' + playerScoreCount)
-          console.log('cpu: ' + computerScoreCount);
-
-          winner = playerChoice;
-          loser = computerChoice;
-
-          playerSelected.style.borderLeft = '20px solid green';
-          computerSelected.style.borderRight = '20px solid red'
-          computerSelected.style.borderRadius = '100px';
-      }
-
-     
-      
-      updateScores();
-
-      checkForWin();
-
-      setTimeout(resetBoard, 1500);
-
-    
-      }
-  
-        
-    
-
-function computerSelection(){
+  } else if ((computerChoice === 'rock' && playerChoice === 'scissors') || (computerChoice === 'scissors' && playerChoice === 'paper') || (computerChoice === 'paper' && playerChoice === 'rock')) {
 
 
-      let randomNumber = Math.floor(Math.random()*3);
-      
-  
-   switch(randomNumber){
-  
-      case 0:
-        computerChoice = 'rock';
-        cpuRock.classList.add('cpu-selected')
-        
-        
-        break;
-  
-      case 1:
-        computerChoice = 'paper';
-        cpuPaper.classList.add('cpu-selected')
-       
-        
-        break;
-  
-      case 2:
-        computerChoice = 'scissors'
-        cpuScissors.classList.add('cpu-selected')
-        
-        
-    }
+    gameInfo.innerText = 'Computer wins the round!';
+    computerScoreCount++;
+    winner = computerChoice;
+    loser = playerChoice;
 
-    computerSelected = document.querySelector('.cpu-selected');
-   
+    playerSelected.style.borderLeft = '20px solid red';
+    computerSelected.style.borderRight = '20px solid green'
+    computerSelected.style.borderRadius = '100px';
+
+  } else {
+
+    gameInfo.innerText = 'You win the round!';
+    playerScoreCount++;
+
+
+
+    winner = playerChoice;
+    loser = computerChoice;
+
+    playerSelected.style.borderLeft = '20px solid green';
+    computerSelected.style.borderRight = '20px solid red'
+    computerSelected.style.borderRadius = '100px';
+  }
+
+
+
+  updateScores();
+
+  checkForWin();
+
+  setTimeout(resetBoard, 1500);
+
+
+}
+
+
+
+
+function computerSelection() {
+
+
+  let randomNumber = Math.floor(Math.random() * 3);
+
+
+  switch (randomNumber) {
+
+    case 0:
+      computerChoice = 'rock';
+      cpuRock.classList.add('cpu-selected')
+
+
+      break;
+
+    case 1:
+      computerChoice = 'paper';
+      cpuPaper.classList.add('cpu-selected')
+
+
+      break;
+
+    case 2:
+      computerChoice = 'scissors'
+      cpuScissors.classList.add('cpu-selected')
+
+
+  }
+
+  computerSelected = document.querySelector('.cpu-selected');
+
 
 }
 
@@ -173,22 +164,22 @@ function updateScores() {
 
 function checkForWin() {
 
-  if (playerScore === 3){
+  if (playerScore === 3) {
     gameInfo.innerText = "You win!"
   } else if (computerScore === 3) {
     gameInfo.innerText = "Computer wins!"
   } else {
     return;
-  } 
+  }
 }
 
-function resetBoard(){
+function resetBoard() {
 
   playerPicks.forEach((pick) => {
     pick.classList.remove('player-selected')
 
     pick.classList.remove('disabled-hover');
-  
+
   })
 
   cpuPicks.forEach((pick) => {
@@ -200,11 +191,11 @@ function resetBoard(){
   playerSelected.style.borderLeft = '';
   computerSelected.style.borderRight = '';
 
+  isRoundPlaying = false;
+
 }
 
-function disableClick(event) {
-  event.preventDefault();
-}
+
 
 
 
